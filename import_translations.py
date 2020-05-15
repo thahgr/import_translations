452a709f87ad1886c82ba747b9485d72150c31ce
Athanasios Chouliaras <athanasios.chouliaras@atos.net>
2020-05-15 14:32:57 +0300

    Added escaping for existing keys

--- import_translations.py
+++ import_translations.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import shutil
 import json
+import re
 from django.utils.encoding import smart_str, smart_unicode
 
 if __name__ == "__main__":
@@ -17,6 +18,8 @@ if __name__ == "__main__":
 	path = sys.argv[2]
 
 	excel = pd.read_excel(file, sheet_name=None, header=None)
+
+	regex = re.compile('<string name="(.*?)">')
 
 	# loop over tabs in the xls file, each tab is a language
 	for lang, sheet in excel.items():
@@ -34,15 +37,26 @@ if __name__ == "__main__":
 		out_file = open(strings, 'w')
 		in_file = open(backupFile, 'r')
 
+		# list with all existing keys for each language
+		keysList = []
+
 		for line in in_file:
 			strLine = line.replace('\n', '')
 			if(strLine != "</resources>"):
 				out_file.write(line)
+				m = regex.search(line)
+				if m:
+					keysList.append(m.group(1))
 			else:
 
 				# we found the end, loop over entries in the language sheet
 				for i in range(len(sheet)) : 
 					key = str(sheet.iloc[i,0])
+
+					# if key already exists, skip it
+					if key in keysList:
+						continue
+
 					english = str(sheet.iloc[i,1])
 					try:
 						translation = smart_str(sheet.iloc[i,2])
